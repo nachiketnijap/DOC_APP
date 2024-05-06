f21af17d0c06e2963066452636d1a91fc4ac32d7
Nachiket <nachiketnijap@gmail.com>
2024-05-06 16:30:23 +0530

    initial commit

---
+++ client/src/pages/BookingPage.jsx
@@ -0,0 +1,147 @@
+import { useParams } from "react-router-dom";
+import Layout from "../components/Layout";
+import React, { useEffect, useState } from "react";
+import { DatePicker, TimePicker, message } from "antd";
+import moment from "moment";
+import { useDispatch, useSelector } from "react-redux";
+import { hideLoading, showLoading } from "../redux/features/alertSlice";
+import axios from "axios";
+
+const BookingPage = () => {
+  const { user } = useSelector((state) => state.user);
+  const params = useParams();
+  const [doctors, setDoctors] = useState([]);
+  const [date, setDate] = useState();
+  const [time, setTime] = useState();
+  const [isAvailable, setIsAvailable] = useState();
+  const dispatch = useDispatch();
+
+  const getUserData = async () => {
+    try {
+      const res = await axios.post(
+        "/api/v1/doctor/getDoctorById",
+        { doctorId: params.doctorId },
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+      if (res.data.success) {
+        setDoctors(res.data.data);
+      }
+    } catch (error) {
+      res.status(500).send({});
+    }
+  };
+
+  const handleBooking = async () => {
+    try {
+      setIsAvailable(true);
+      if (!date && !time) {
+        return alert("date and time required");
+      }
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/user/book-appointment",
+        {
+          doctorId: params.doctorId,
+          userId: user._id,
+          doctorInfo: doctors,
+          date: date,
+          userInfo: user,
+          time: time,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (res.data.success) {
+        message.success(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+    }
+  };
+
+  const handleAvailability = async () => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/user/booking-availability",
+        { doctorId: params.doctorId, date, time },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (res.data.success) {
+        setIsAvailable(true);
+        message.success(res.data.message);
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+  return (
+    <Layout>
+      <h1>Booking Page</h1>
+      <div className="container m-2">
+        {doctors && (
+          <div>
+            <h4>
+              Dr.{doctors.firstName} {doctors.lastName}
+            </h4>
+            <h4>Fees: {doctors.feesPerConsultation}</h4>
+            <h4>
+              Timings: {doctors.timings && doctors.timings[0]} -{" "}
+              {doctors.timings && doctors.timings[1]}
+            </h4>
+            <div className="d-flex flex-column w-50">
+              <DatePicker
+                className="m-2"
+                format="DD-MM-YYYY"
+                onChange={(value) => {
+                  setIsAvailable(true);
+                  setDate(moment(value).format("DD-MM-YYYY"));
+                }}
+              />
+              <TimePicker
+                className="m-2"
+                format="HH:mm"
+                onChange={(value) => {
+                  setIsAvailable(true);
+                  setTime(moment(value).format("HH:mm"));
+                }}
+              />
+              <button
+                className="btn btn-primary mt-2"
+                onClick={handleAvailability}
+              >
+                Check Availability
+              </button>
+              {isAvailable && (
+                <button className="btn btn-dark mt-2" onClick={handleBooking}>
+                  Book Now
+                </button>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+export default BookingPage;
---
+++ client/src/pages/doctor/DoctorAppointment.jsx
@@ -0,0 +1,113 @@
+import Layout from "../../components/Layout";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import moment from "moment";
+import { Table } from "antd";
+const DoctorAppointment = () => {
+  const [appointments, setAppointments] = useState([]);
+  const getAppointments = async () => {
+    try {
+      const res = await axios.get("/api/v1/doctor/doctor-appointments", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (res.data.success) {
+        setAppointments(res.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({});
+    }
+  };
+  const handleStatus = async (record, status) => {
+    try {
+      const res = await axios.post(
+        "/api/v1/doctor/update-status",
+        { appointmentsId: record._id, status },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (res.data.success) {
+        message.success(res.data.message);
+        getAppointments();
+      }
+    } catch (error) {}
+  };
+  useEffect(() => {
+    getAppointments();
+  }, []);
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "_id",
+    },
+    // {
+    //   title: "Name",
+    //   dataIndex: "name",
+    //   render: (text, record) => (
+    //     <span>
+    //       {record.doctorId.firstName} {record.doctorId.lastName}
+    //     </span>
+    //   ),
+    // },
+    // {
+    //   title: "Phone",
+    //   dataIndex: "phone",
+    //   render: (text, record) => <span>{record.doctorId.phone}</span>,
+    // },
+    {
+      title: "Date",
+      dataIndex: "date",
+      render: (text, record) => (
+        <span>
+          {moment(record.date).format("DD-MM-YYYY")}{" "}
+          {/* {moment(record.time).format("HH:mm")} */}
+        </span>
+      ),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <div className="d-flex">
+          {record.status === "pending" && (
+            <div className="d-flex">
+              <button
+                className="btn btn-success"
+                onClick={() => handleStatus(record, "approve")}
+              >
+                Approve
+              </button>
+
+              <button
+                className="btn btn-danger mx-2"
+                onClick={() => handleStatus(record, "reject")}
+              >
+                Reject
+              </button>
+            </div>
+          )}
+        </div>
+      ),
+    },
+  ];
+  return (
+    <div>
+      <Layout>
+        <h1>Appointments</h1>
+        <Table columns={columns} dataSource={appointments} />
+      </Layout>
+    </div>
+  );
+};
+
+export default DoctorAppointment;
